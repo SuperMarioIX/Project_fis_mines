fb76761e032f2bb200bda010ead57bfac3bde6e1
Cebanu Dumitrita <dumitrita.cebanu.99@gmail.com>
2021-05-16 15:21:35 +0300

    Write fetch function for Leaderboard json

--- src/main/java/controllers/Leaderboard.java
+++ src/main/java/controllers/Leaderboard.java
@@ -8,7 +8,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
@@ -27,6 +31,21 @@ public class Leaderboard implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+         JSONArray board = readFile("src/main/java/sources/board.json");
+        System.out.println(board);
+    }
 
+    public JSONArray readFile(String sourceFilePath){
+
+        JSONParser parser = new JSONParser();
+        Object obj = null;
+
+        try {
+            obj = parser.parse(new FileReader(sourceFilePath));
+        } catch (IOException | ParseException exception) {
+            exception.printStackTrace();
+        }
+        JSONArray userList = (JSONArray) obj;
+        return userList;
     }
 }
