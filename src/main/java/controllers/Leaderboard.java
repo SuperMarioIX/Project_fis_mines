2efb766d3bcd72c5bdcece1a7ff21f79f3346155
Cebanu Dumitrita <dumitrita.cebanu.99@gmail.com>
2021-05-16 17:34:17 +0300

    Show Leaderboard content to Front

--- src/main/java/controllers/Leaderboard.java
+++ src/main/java/controllers/Leaderboard.java
@@ -7,11 +7,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
@@ -19,6 +26,13 @@ import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Leaderboard implements Initializable {
+    @FXML
+     TableView<User> boardTable;
+    @FXML
+    private TableColumn<User, String> boardTableUsername;
+    @FXML
+    private TableColumn<User, String> boardTableTime;
+    public String dndpath = "src/main/java/sources/board.json";
 
     @FXML
     public void Back(ActionEvent event) throws IOException {
@@ -31,10 +45,35 @@ public class Leaderboard implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-         JSONArray board = readFile("src/main/java/sources/board.json");
+        JSONArray board = readFile("src/main/java/sources/board.json");
         System.out.println(board);
 
+        boardTableUsername.setCellValueFactory(new PropertyValueFactory<User, String>("Username"));
+        boardTableTime.setCellValueFactory(new PropertyValueFactory<User, String>("Highscore"));
 
+        boardTableUsername.setCellFactory(TextFieldTableCell.forTableColumn());
+        boardTableTime.setCellFactory(TextFieldTableCell.forTableColumn());
+        //boardTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        JSONParser parser = new JSONParser();
+        Object obj;
+        JSONArray array = new JSONArray();
+
+        try{
+            FileReader readFile = new FileReader(dndpath);
+            BufferedReader buffer = new BufferedReader(readFile);
+            obj = parser.parse(buffer);
+            if(obj instanceof JSONArray){
+                array = (JSONArray) obj;
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+        for(JSONObject table : (Iterable<JSONObject>) array){
+            User users = new User((String) ((JSONObject) table).get("User"), (String) ((JSONObject) table).get("Score"));
+            boardTable.getItems().add(users);
+            users.getUserData();
+        }
     }
 
     public JSONArray readFile(String sourceFilePath){
@@ -47,7 +86,6 @@ public class Leaderboard implements Initializable {
         } catch (IOException | ParseException exception) {
             exception.printStackTrace();
         }
-        JSONArray userList = (JSONArray) obj;
-        return userList;
+        return (JSONArray) obj;
     }
 }
